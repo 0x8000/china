c3dc4086956ea5e63e27076d06c16f6445ad65a9
0x8000 <0x8000@users.noreply.github.com>
2020-12-26 17:35:49 +0100

    Draw in one colour without sharing and resizing

--- javascript.js
+++ javascript.js
@@ -15,6 +15,13 @@ var sendButton = document.getElementById("send-button");
 var MY_ID = "";
 var PEER_ID = "";
 
+var DRAWING = false;
+var DRAWING_DATA = {
+    x: 0,
+    y: 0,
+    colour: "#FF0000",
+};
+
 // Happens to all
 function init() {
     peer = new Peer(null, {});
@@ -42,7 +49,7 @@ function init() {
 
     let localStream = null;
     const startChat = async () => {
-    localStream = await navigator.mediaDevices.getUserMedia({video: true})
+        localStream = await navigator.mediaDevices.getUserMedia({ video: true })
         document.querySelector("video#local").srcObject = localStream;
     }
     startChat();
@@ -115,8 +122,65 @@ function addMessage(sender, message) {
     document.getElementById("received-messages").appendChild(newLiElement);
 }
 
+function drawLine(x0, y0, x1, y1, colour, emit) {
+    var context = document.getElementById("whiteboard").getContext("2d");
+    context.beginPath();
+    context.moveTo(x0, y0);
+    context.lineTo(x1, y1);
+    context.strokeStyle = colour;
+    context.lineWidth = 2;
+    context.stroke();
+    context.closePath();
+
+    if (emit) {
+        // TODO: Form data and send it
+    }
+}
+
+function drawGetX(e) {
+    var rect = document.getElementById("whiteboard").getBoundingClientRect();
+    return e.clientX - rect.left || e.touches[0].clientX - rect.left;
+}
+
+function drawGetY(e) {
+    var rect = document.getElementById("whiteboard").getBoundingClientRect();
+    return e.clientY - rect.top || e.touches[0].clientY - rect.top;
+}
+
+function onMouseDown(e) {
+    DRAWING = true;
+    DRAWING_DATA.x = drawGetX(e);
+    DRAWING_DATA.y = drawGetY(e);
+}
+
+function onMouseUp(e) {
+    if (!DRAWING) { return; }
+    DRAWING = false;
+    drawLine(DRAWING_DATA.x, DRAWING_DATA.y, drawGetX(e), drawGetY(e), DRAWING_DATA.colour, true);
+}
+
+function onMouseMove(e) {
+    if (!DRAWING) { return; }
+    drawLine(DRAWING_DATA.x, DRAWING_DATA.y, drawGetX(e), drawGetY(e), DRAWING_DATA.colour, true);
+    DRAWING_DATA.x = drawGetX(e);
+    DRAWING_DATA.y = drawGetY(e);
+}
+
+// FIXME: Get corrent size as column + drawing disapeer after resize
+function onResize() {
+    document.getElementById("whiteboard").width = document.getElementById("drawing-area").offsetWidth;
+    document.getElementById("whiteboard").height = document.getElementById("drawing-area").offsetHeight;
+}
+
 window.addEventListener("load", function () {
     document.getElementById("connect-button").addEventListener("click", join);
     document.getElementById("send-button").addEventListener("click", send);
     init();
+
+    document.getElementById("whiteboard").addEventListener("mousedown", onMouseDown, false);
+    document.getElementById("whiteboard").addEventListener("mouseup", onMouseUp, false);
+    document.getElementById("whiteboard").addEventListener("mouseout", onMouseUp, false);
+    document.getElementById("whiteboard").addEventListener("mousemove", onMouseMove, false);
+
+    window.addEventListener("resize", onResize, false);
 });
